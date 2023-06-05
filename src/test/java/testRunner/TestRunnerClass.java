5693d722319eaf6c45d9f53f0de75799a90a850e
shivani17april <shivani.sharma@antiersolutions.com>
2023-06-05 22:32:10 +0530

    Added Surefire plugin into pom

--- src/test/java/testRunner/TestRunnerClass.java
+++ src/test/java/testRunner/TestRunnerClass.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/features/talkToExperts.feature",
-                 glue="stepDefintion",
-                 dryRun = true
+                 glue="stepDefintion"
+
 )
 public class TestRunnerClass extends AbstractTestNGCucumberTests {
 
